38c7e7e6909cdd33cff6622ab4bf7d6c7a60123a
markjconlon <markjconlon@gmail.com>
2019-04-06 15:58:40 -0400

    wrote calculateWinner to check win condition and display as status instead of Next player

--- src/index.js
+++ src/index.js
@@ -21,31 +21,54 @@ function Board() {
           newSquares[i] = xTurn ? 'X' : 'O';
           let newTurn = !xTurn;
           setTurn(newTurn);
-          setSquares(newSquares) }}/>
+          setSquares(newSquares) }
+        }
+      />
     );
   }
-    const status = 'Next player: X';
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
+  function calculateWinner(squares){
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const winner = winningCombinations.filter(lines => (
+      squares[lines[0]] === squares[lines[1]] &&
+      squares[lines[0]] === squares[lines[2]] &&
+      ["X", "O"].includes(squares[lines[0]]) )
+      );
+    return winner.length > 0 ? `Winner is ${squares[winner[0][0]]}` : `Next player: ${xTurn ? 'X' : 'O'}`;
+  }
+
+  const status = calculateWinner(squares);
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 function Game() {
